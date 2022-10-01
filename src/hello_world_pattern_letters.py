61457a6321e2b6dea97f761b38223000bcdae394
Aaryansh Sahay <53135035+aaryanshsahay@users.noreply.github.com>
2022-10-01 23:17:32 +0530

    Create hello_world_pattern_letters.py
    displaying hello world with every letter made up of itself

---
+++ src/hello_world_pattern_letters.py
@@ -0,0 +1,101 @@
+
+def make_h():
+    return '''
+            H     H
+            H     H
+            H     H
+            H H H H
+            H     H
+            H     H
+            H     H
+            H     H
+            '''
+
+
+
+def make_e():
+    return '''
+            E E E E
+            E
+            E
+            E E E
+            E
+            E
+            E
+            E E E E
+            '''
+
+def make_l():
+    return '''
+            L
+            L
+            L
+            L
+            L
+            L
+            L
+            L L L L 
+            '''
+
+
+def make_o():
+    return '''
+               O
+            O     O
+            O     O
+            O     O
+            O     O
+            O     O
+               O
+            '''
+
+def make_w():
+    return '''
+            W          W
+            W          W
+            W          W
+            W          W
+            W    WW    W
+            W   W  W   W
+             W W    W W 
+              W      W
+            '''
+
+def make_r():
+    return '''
+            R R R R
+            R     R
+            R     R
+            R R R R
+            R  R
+            R   R
+            R    R
+            R     R
+            '''
+
+def make_d():
+    return '''
+            D D D
+            D    D
+            D     D
+            D      D
+            D      D
+            D     D
+            D    D
+            D D D
+            '''
+
+
+if __name__ == "__main__":
+    print(make_h())
+    print(make_e())
+    print(make_l())
+    print(make_l())
+    print(make_o())
+    print(' ')
+    print(' ')
+    print(make_w())
+    print(make_o())
+    print(make_r())
+    print(make_l())
+    print(make_d())
